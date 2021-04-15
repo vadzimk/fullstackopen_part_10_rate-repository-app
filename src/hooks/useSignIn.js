8ed52de980e71dd8a23371ef1d213a497cf98ff8
Vadzim <vadzimk@hotmail.com>
2021-04-14 17:54:03 -0700

    10.14 storing access token step1

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,14 +1,19 @@
 import {useMutation} from "@apollo/client";
 import {AUTHORIZE} from "../graphql/queries.js";
+import AuthStorage from "../utils/authStorage.js";
 
 const useSignIn = () => {
     const [sendCredentials, {data}] = useMutation(AUTHORIZE);
+    const authStorage = new AuthStorage();
 
     const signIn = async ({username, password}) => {
-        const credentials  = {username, password};
+        const credentials = {username, password};
         await sendCredentials({variables: {credentials}});
-
+        if (data && data.authorize)
+            await authStorage.setAccessToken(data.authorize.accessToken);
     };
+
+
 
     return [signIn, data];
 };
