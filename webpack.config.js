bb361003e931623b6bf69fb4d30a58df02e437f6
Vadzim <vadzimk@hotmail.com>
2021-06-09 21:33:53 -0700

    fixed es6 modules bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-import path from 'path';
-import createExpoWebpackConfigAsync from '@expo/webpack-config';
-export default async function(env, argv){
+const path = require('path');
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+module.exports = async function(env, argv) {
     const config = await createExpoWebpackConfigAsync(env, argv);
     config.module.rules.push({
         test: /\.js$/,
